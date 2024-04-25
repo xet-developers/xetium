e3d3b5b921d4d0bf3de35201602e71733b0644c2
nenichv <alena_volkova_2004@mail.ru>
2024-04-25 22:25:58 +0500

    sidebar is ready

--- xetiumapi.client/src/shared/ui/components/Menu/Menu.tsx
+++ xetiumapi.client/src/shared/ui/components/Menu/Menu.tsx
@@ -1,71 +1,49 @@
-import {Link} from "react-router-dom";
-import {Icon} from "@/shared/ui/components/Icon";
-import IconPersonalAccount from "../../assets/account.svg";
-import IconEllipse from '../../assets/ellipse.svg';
-import IconPlus from "../../assets/plus.svg";
-import IconMyProject from "../../assets/my-project.svg";
-import IconReport from "../../assets/report.svg";
-import IconPosition from "../../assets/position.svg";
-import IconSchedule from "../../assets/schedule.svg";
-import IconClustering from "../../assets/clusterung.svg";
-import IconAuthogeneration from "../../assets/authogeneration.svg";
+import {
+    UserOutlined,
+    ProductOutlined,
+    PlusCircleOutlined,
+    ProfileOutlined,
+    FileTextOutlined,
+    MonitorOutlined,
+    ScheduleOutlined,
+    PartitionOutlined,
+    ClusterOutlined,
+    RightOutlined } from '@ant-design/icons';
 
-import cls from "../Menu/Menu.module.scss";
-import {Menu} from "antd";
-
-interface MenuItemTypeInterface {
+type MenuItem = {
+    key: React.Key;
     label: string;
-    type: 'item';
-    link: string;
-    icon: string;
-}
-
-interface SubMenuTypeInterface {
-    label: string;
-    type: 'submenu';
-    items: MenuItemTypeInterface[];
-}
-
-type MenuItem = MenuItemTypeInterface | SubMenuTypeInterface;
+    link?: string;
+    icon?: React.ReactNode;
+    children?: MenuItem[];
+};
 
 export const MenuItems: MenuItem[] = [
-    { label: 'Личный кабинет', link: '/personalaccount', type: 'item', icon: IconPersonalAccount },
-    {
-        label: 'Имя пространства',
-        type: 'submenu',
-        items: [
-            { label: 'Пространство 1', link: '/myproject', type: 'item', icon: IconEllipse },
-            { label: 'Добавить пространство', link: '/myproject', type: 'item', icon: IconPlus }, //сделать кнопкой и сабмит
-        ],
-    },
-    { label: 'Мой проект', link: '/myproject', type: 'item', icon: IconMyProject },
-    { label: 'Отчеты', link: '/reports', type: 'item', icon: IconReport },
-    { label: 'Проверка позиций сайта', link: '/checksitepositions', type: 'item', icon: IconPosition },
-    { label: 'Расписание проверки позиций', link: '/analysistoschedule', type: 'item', icon: IconSchedule },
-    { label: 'Кластеризация запросов', link: '/queryclustering', type: 'item', icon: IconClustering },
-    { label: 'Автогенерация запросов', link: '/autoquerygeneration', type: 'item', icon: IconAuthogeneration },
+    getItem('1','Личный кабинет', '/personalaccount', <UserOutlined style={{ color: '#ffffff' }}/>),
+    getItem('0','Имя пространства', '/', <ProductOutlined style={{ color: '#ffffff' }}/>,[
+        getItem('2','Пространство 1', '/', <RightOutlined style={{ color: '#ffffff' }}/>),
+        getItem('3','Добавить пространство', '/', <PlusCircleOutlined style={{ color: '#ffffff' }}/>),
+    ]),
+    getItem('4','Мой проект', '/myproject', <ProfileOutlined style={{ color: '#ffffff' }}/>),
+    getItem('5','Отчеты', '/reports', <FileTextOutlined style={{ color: '#ffffff' }}/>),
+    getItem('6','Проверка позиций сайта', '/checksitepositions', <MonitorOutlined style={{ color: '#ffffff' }}/>),
+    getItem('7','Расписание проверки позиций', '/analysistoschedule', <ScheduleOutlined style={{ color: '#ffffff' }}/>),
+    getItem('8','Кластеризация запросов', '/queryclustering', <PartitionOutlined style={{ color: '#ffffff' }}/>),
+    getItem('9','Автогенерация запросов', '/autoquerygeneration', <ClusterOutlined style={{ color: '#ffffff' }}/>),
 ];
 
-export const RenderMenuItem = (item: MenuItem, index: number) => {
-
-    if (item.type === 'item') {
-        return (
-            <div>
-                <Link to={item.link} key={index} className={cls.link}>
-                    <div className={cls.i}>
-                        <Icon src={item.icon}/>
-                        <Menu.Item className={cls.submenu__item}>{item.label}</Menu.Item>
-                    </div>
-                </Link>
-            </div>
-
-        );
-    } else if (item.type === 'submenu') {
-        return (
-            <Menu.SubMenu key={index} title={item.label} className={cls.submenu}>
-                {item.items.map(RenderMenuItem)}
-            </Menu.SubMenu>
-        );
-    }
-    return null;
-};
+export function getItem(
+    key: React.Key,
+    label: string,
+    link: string,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+): MenuItem {
+    return {
+        key,
+        label,
+        link,
+        icon,
+        children,
+    } as MenuItem;
+}
