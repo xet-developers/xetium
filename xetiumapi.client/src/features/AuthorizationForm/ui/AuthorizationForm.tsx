a0c7081ee4635faf5ee160715f03e29dfeacd85f
nenichv <alena_volkova_2004@mail.ru>
2024-05-03 00:29:44 +0500

    scss authorization

--- xetiumapi.client/src/features/AuthorizationForm/ui/AuthorizationForm.tsx
+++ xetiumapi.client/src/features/AuthorizationForm/ui/AuthorizationForm.tsx
@@ -9,9 +9,10 @@ import {useSelector} from "react-redux";
 import {getPassword, getUserName} from "../model/selectors/AuthorizationForm.selectors.ts";
 import {useAuthorizationFormApi} from "@/features/AuthorizationForm/api/AuthorizationForm.api.ts";
 import {USER_LOCALSTORAGE_KEY} from "@/shared/const/localstorage.ts";
+import cls from "../styles/AuthorizationForm.module.scss";
 
-import {Button, Flex, Input} from "antd";
-import {StyleProvider} from '@ant-design/cssinjs';
+import {Button, Flex, Input, Typography } from "antd";
+const { Title, Link } = Typography;
 
 interface IAuthorizationFormProps {
 }
@@ -62,17 +63,31 @@ export const AuthorizationForm: FC<IAuthorizationFormProps> = () => {
 
     return (
         <DynamicModuleLoader reducers={reducers}>
-            <Flex justify={'center'} align={"center"}>
-                <Flex vertical={true} justify={'center'} gap={5}>
-                    <Input onChange={(value: string) => onChangeUsername(value)}
-                           placeholder={'введите Имя'}/>
-                    <Input onChange={(value: string) => onChangePassword(value)}
-                           placeholder={'введите пароль'}/>
+            <div className={cls.page}>
+                <Flex justify={"center"} align={"center"} className={cls.form}>
+                    <Flex vertical={true} gap={30} justify={"center"} align={"center"}>
+                        <Title level={2} style={{marginTop: '40px'}}>Авторизация</Title>
+                        <Flex justify={'center'} gap={5} style={{marginTop: '-40px', paddingBottom: '40px'}}>
+                            <Title level={5} style={{color: '#5F5F5F'}}>Еще нет аккаунта?</Title>
+                            <Link href="/registration">
+                                <Title level={5} style={{color: '#F66450'}}>
+                                    Зарегистрироваться
+                                </Title>
+                            </Link>
+                        </Flex>
 
-                    <Button onClick={onRegisterClick}>Регистрация</Button>
-                    {isLoading && <p>Loading...</p>}
+                        <Flex vertical={true} justify={'center'} gap={40}>
+                            <Input onChange={(value: string) => onChangeUsername(value)}
+                                   placeholder={'Имя пользователя или почта'} className={cls.input}/>
+                            <Input onChange={(value: string) => onChangePassword(value)}
+                                   placeholder={'Пароль'} className={cls.input}/>
+                        </Flex>
+
+                        <Button type="primary" onClick={onRegisterClick} className={cls.btn}>Перейти к проектам</Button>
+                        {isLoading && <p>Loading...</p>}
+                    </Flex>
                 </Flex>
-            </Flex>
+            </div>
         </DynamicModuleLoader>
     );
 };
