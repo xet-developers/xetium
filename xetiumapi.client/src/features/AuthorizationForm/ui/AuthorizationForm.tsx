597066591e26d7c4a591085f2bf2e7a0f2f3d524
nenichv <alena_volkova_2004@mail.ru>
2024-05-07 00:11:35 +0500

    scss authorization

--- xetiumapi.client/src/features/AuthorizationForm/ui/AuthorizationForm.tsx
+++ xetiumapi.client/src/features/AuthorizationForm/ui/AuthorizationForm.tsx
@@ -11,7 +11,7 @@ import {useAuthorizationFormApi} from "@/features/AuthorizationForm/api/Authoriz
 import {USER_LOCALSTORAGE_KEY} from "@/shared/const/localstorage.ts";
 import cls from "../styles/AuthorizationForm.module.scss";
 
-import {Button, Flex, Input, Typography } from "antd";
+import {ConfigProvider, Button, Flex, Input, Typography } from "antd";
 const { Title, Link } = Typography;
 
 interface IAuthorizationFormProps {
@@ -63,31 +63,56 @@ export const AuthorizationForm: FC<IAuthorizationFormProps> = () => {
 
     return (
         <DynamicModuleLoader reducers={reducers}>
-            <div className={cls.page}>
-                <Flex justify={"center"} align={"center"} className={cls.form}>
-                    <Flex vertical={true} gap={30} justify={"center"} align={"center"}>
-                        <Title level={2} style={{marginTop: '40px'}}>Авторизация</Title>
-                        <Flex justify={'center'} gap={5} style={{marginTop: '-40px', paddingBottom: '40px'}}>
-                            <Title level={5} style={{color: '#5F5F5F'}}>Еще нет аккаунта?</Title>
-                            <Link href="/registration">
-                                <Title level={5} style={{color: '#F66450'}}>
-                                    Зарегистрироваться
+            <ConfigProvider
+                theme={{
+                    token: {
+                        colorPrimary: '#F66450',
+                        fontFamilyCode: 'Montserrat Alternates',
+                        colorText: '#252525'
+                    },
+                    components: {
+                        Button: {
+                            defaultHoverColor: '#F66450'
+                        },
+                    },
+                }}
+            >
+                <div className={cls.page}>
+                    <Flex justify={"center"} align={"center"} className={cls.form}>
+                        <Flex vertical={true} gap={30} justify={"center"} align={"center"}>
+                            <Title level={2} style={{marginTop: '40px', fontSize: '40px', fontFamily: 'Montserrat'}}>Авторизация</Title>
+                            <Flex justify={'center'} gap={5} style={{marginTop: '-40px', paddingBottom: '40px'}}>
+                                <Title level={5} style={{color: '#5F5F5F', fontSize: '15px', fontFamily: 'Montserrat'}}>Еще нет аккаунта?</Title>
+                                <Link href="/registration">
+                                    <Title level={5} style={{color: '#F66450', fontSize: '15px', fontFamily: 'Montserrat'}} className={cls.hoverText}>
+                                        Зарегистрироваться
+                                    </Title>
+                                </Link>
+                            </Flex>
+
+                            <Flex vertical={true} justify={'center'} gap={40}>
+                                <Input onChange={(value: string) => onChangeUsername(value)}
+                                       placeholder={'Имя пользователя или почта'} className={cls.input}/>
+                                <Input onChange={(value: string) => onChangePassword(value)}
+                                       placeholder={'Пароль'} className={cls.input}/>
+                            </Flex>
+
+                            {isLoading ? (
+                                <Title level={5} style={{color: '#252525', marginTop: '10px', fontSize: '18px', fontWeight: '400', fontFamily: 'Montserrat'}}>
+                                    Загрузка...
                                 </Title>
-                            </Link>
+                            ) : (
+                                <Button type="primary" onClick={onRegisterClick} className={cls.btn}>
+                                    <Title level={5} style={{color: '#fff', marginTop: '10px', fontSize: '18px', fontWeight: '400', fontFamily: 'Montserrat'}}>
+                                        ПЕРЕЙТИ К ПРОЕКТАМ
+                                    </Title>
+                                </Button>
+                            )}
                         </Flex>
-
-                        <Flex vertical={true} justify={'center'} gap={40}>
-                            <Input onChange={(value: string) => onChangeUsername(value)}
-                                   placeholder={'Имя пользователя или почта'} className={cls.input}/>
-                            <Input onChange={(value: string) => onChangePassword(value)}
-                                   placeholder={'Пароль'} className={cls.input}/>
-                        </Flex>
-
-                        <Button type="primary" onClick={onRegisterClick} className={cls.btn}>Перейти к проектам</Button>
-                        {isLoading && <p>Loading...</p>}
                     </Flex>
-                </Flex>
-            </div>
+                </div>
+            </ConfigProvider>
+
         </DynamicModuleLoader>
     );
 };
