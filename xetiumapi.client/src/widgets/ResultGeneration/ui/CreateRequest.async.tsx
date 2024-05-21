4ab35cf5526d26a134bd661589616e2ea33c356c
nenichv <alena_volkova_2004@mail.ru>
2024-05-21 19:36:12 +0500

    template result generation

--- xetiumapi.client/src/widgets/ResultGeneration/ui/CreateRequest.async.tsx
+++
@@ -1,6 +0,0 @@
-import {type ComponentType, lazy} from "react";
-
-export const CreateRequestAsync = lazy<ComponentType>(
-    async () => await import('./CreateRequest')
-        .then((module) => ({default: module.CreateRequest})),
-);
