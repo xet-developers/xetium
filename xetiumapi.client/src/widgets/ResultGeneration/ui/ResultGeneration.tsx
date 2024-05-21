0c0b92075a8c359848c2cb7d98f6274632a27ee7
nenichv <alena_volkova_2004@mail.ru>
2024-05-21 19:51:18 +0500

    style result generation

--- xetiumapi.client/src/widgets/ResultGeneration/ui/ResultGeneration.tsx
+++ xetiumapi.client/src/widgets/ResultGeneration/ui/ResultGeneration.tsx
@@ -3,81 +3,113 @@ import  { useState } from 'react';
 import { Button, ConfigProvider, Table, TableProps  } from "antd";
 import { DownloadOutlined } from '@ant-design/icons';
 
-const [loadings, setLoadings] = useState<boolean[]>([]);
-
-const enterLoading = (index: number) => {
-    setLoadings((prevLoadings) => {
-        const newLoadings = [...prevLoadings];
-        newLoadings[index] = true;
-        return newLoadings;
-    });
-
-    setTimeout(() => {
-        setLoadings((prevLoadings) => {
-            const newLoadings = [...prevLoadings];
-            newLoadings[index] = false;
-            return newLoadings;
-        });
-    }, 6000);
-};
-
-interface DataType {
-    key: string;
-    navigate: string;
-    info: string;
-}
-
-const columns: TableProps<DataType>['columns'] = [
-    {
-        title: 'Навигационный',
-        dataIndex: 'navigate',
-        key: 'navigate',
-    },
-    {
-        title: 'Информационный',
-        dataIndex: 'info',
-        key: 'info',
-    }
-];
-
-const data: DataType[] = [
-    {
-        key: '1',
-        navigate: 'Какой магазин предлагает утюги с техническими характеристиками?',
-        info: 'Какой магазин предлагает утюги с техническими характеристиками?'
-    },
-    {
-        key: '1',
-        navigate: 'Какой магазин предлагает утюги с техническими характеристиками?',
-        info: 'Какой магазин предлагает утюги с техническими характеристиками?'
-    },
-    {
-        key: '1',
-        navigate: 'Какой магазин предлагает утюги с техническими характеристиками?',
-        info: 'Какой магазин предлагает утюги с техническими характеристиками?'
-    },
-];
 
 export const ResultGeneration = () => {
+
+    const [loadings, setLoadings] = useState<boolean[]>([]);
+
+    const enterLoading = (index: number) => {
+        setLoadings((prevLoadings) => {
+            const newLoadings = [...prevLoadings];
+            newLoadings[index] = true;
+            return newLoadings;
+        });
+
+        setTimeout(() => {
+            setLoadings((prevLoadings) => {
+                const newLoadings = [...prevLoadings];
+                newLoadings[index] = false;
+                return newLoadings;
+            });
+        }, 6000);
+    };
+
+    interface DataType {
+        key: string;
+        navigate: string;
+        info: string;
+    }
+
+    const columns: TableProps<DataType>['columns'] = [
+        {
+            title: 'Навигационный',
+            dataIndex: 'navigate',
+            key: 'navigate',
+        },
+        {
+            title: 'Информационный',
+            dataIndex: 'info',
+            key: 'info',
+        }
+    ];
+
+    const data: DataType[] = [
+        {
+            key: '1',
+            navigate: 'Какой магазин предлагает утюги с техническими характеристиками?',
+            info: 'Какой магазин предлагает утюги с техническими характеристиками?'
+        },
+        {
+            key: '1',
+            navigate: 'Какой магазин предлагает утюги с техническими характеристиками?',
+            info: 'Какой магазин предлагает утюги с техническими характеристиками?'
+        },
+        {
+            key: '1',
+            navigate: 'Какой магазин предлагает утюги с техническими характеристиками?',
+            info: 'Какой магазин предлагает утюги с техническими характеристиками?'
+        },
+        {
+            key: '1',
+            navigate: 'Какой магазин предлагает утюги с техническими характеристиками?',
+            info: 'Какой магазин предлагает утюги с техническими характеристиками?'
+        },
+        {
+            key: '1',
+            navigate: 'Какой магазин предлагает утюги с техническими характеристиками?',
+            info: 'Какой магазин предлагает утюги с техническими характеристиками?'
+        },
+        {
+            key: '1',
+            navigate: 'Какой магазин предлагает утюги с техническими характеристиками?',
+            info: 'Какой магазин предлагает утюги с техническими характеристиками?'
+        },
+        {
+            key: '1',
+            navigate: 'Какой магазин предлагает утюги с техническими характеристиками?',
+            info: 'Какой магазин предлагает утюги с техническими характеристиками?'
+        },
+        {
+            key: '1',
+            navigate: 'Какой магазин предлагает утюги с техническими характеристиками?',
+            info: 'Какой магазин предлагает утюги с техническими характеристиками?'
+        },
+    ];
+
 
     return (
         <ConfigProvider
             theme={{
                 token: {
                     colorPrimary: '#F66450',
-                    colorText: '#252525'
+                    colorText: '#252525',
+                    fontFamily: 'Montserrat',
+                    lineHeight: 0.8
                 },
                 components: {
                     Button: {
                         textHoverBg: '#ba4c3b',
                         defaultActiveBg: '#ba4c3b'
+                    },
+                    Table: {
+                        headerColor: '#252525',
                     }
                 }
             }}
         >
             <div className={cls.container}>
-                <div>
-                    <span className={cls.header}>Результаты</span>
+                <div className={cls.header}>
+                    <span>Результаты</span>
                     <Button
                         type="primary"
                         icon={<DownloadOutlined/>}
@@ -89,7 +121,7 @@ export const ResultGeneration = () => {
                     </Button>
                 </div>
 
-                <Table className={cls.blockData} columns={columns} dataSource={data} />;
+                <Table className={cls.blockData} columns={columns} dataSource={data} />
             </div>
         </ConfigProvider>
     );
