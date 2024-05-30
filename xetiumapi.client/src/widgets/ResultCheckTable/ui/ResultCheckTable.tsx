88afffcf4ae128f9803bbfb166df6d0e079136f5
nenichv <alena_volkova_2004@mail.ru>
2024-05-30 19:58:04 +0500

    style my project

--- xetiumapi.client/src/widgets/ResultCheckTable/ui/ResultCheckTable.tsx
+++ xetiumapi.client/src/widgets/ResultCheckTable/ui/ResultCheckTable.tsx
@@ -1,13 +1,158 @@
 import cls from "./ResultCheckTable.module.scss";
 import  { useState, useEffect } from 'react';
-import { Button, ConfigProvider, Table, TableProps, Skeleton } from "antd";
-import { DownloadOutlined } from '@ant-design/icons';
+import { ConfigProvider, Table, TableColumnsType, Skeleton } from "antd";
 
 
 export const ResultCheckTable = () => {
 
     const [loadings, setLoadings] = useState<boolean[]>([]);
     const [generation, setGeneration] = useState(true);
+
+    interface DataType {
+        key: React.Key;
+        name: string;
+        age: number;
+        street: string;
+        building: string;
+        number: number;
+        companyAddress: string;
+        companyName: string;
+        gender: string;
+    }
+
+    const columns: TableColumnsType<DataType> = [
+        {
+            title: 'Ключевые фразы',
+            dataIndex: 'name',
+            key: 'name',
+            width: 250,
+            fixed: 'left',
+            onFilter: (value, record) => record.name.indexOf(value as string) === 0,
+        },
+        {
+            title: 'Дата и время',
+            children: [
+                {
+                    title: '30.05.24 в 06:00',
+                    children: [
+                        {
+                            title: 'Яндекс',
+                            dataIndex: 'building',
+                            key: 'building',
+                            width: 100,
+                        },
+                        {
+                            title: 'Google',
+                            dataIndex: 'number',
+                            key: 'number',
+                            width: 100,
+                        },
+                    ],
+
+                },
+                {
+                    title: '30.05.24',
+                    children: [
+                        {
+                            title: 'Яндекс',
+                            dataIndex: 'building',
+                            key: 'building',
+                            width: 100,
+                        },
+                        {
+                            title: 'Google',
+                            dataIndex: 'number',
+                            key: 'number',
+                            width: 100,
+                        },
+                    ],
+                },
+                {
+                    title: '30.05.24',
+                    children: [
+                        {
+                            title: 'Яндекс',
+                            dataIndex: 'building',
+                            key: 'building',
+                            width: 100,
+                        },
+                        {
+                            title: 'Google',
+                            dataIndex: 'number',
+                            key: 'number',
+                            width: 100,
+                        },
+                    ],
+                },
+                {
+                    title: '30.05.24 в 17:00',
+                    children: [
+                        {
+                            title: 'Яндекс',
+                            dataIndex: 'building',
+                            key: 'building',
+                            width: 100,
+                        },
+                        {
+                            title: 'Google',
+                            dataIndex: 'number',
+                            key: 'number',
+                            width: 100,
+                        },
+                    ],
+                },
+                {
+                    title: '30.05.24 в 12:00',
+                    children: [
+                        {
+                            title: 'Яндекс',
+                            dataIndex: 'building',
+                            key: 'building',
+                            width: 100,
+                        },
+                        {
+                            title: 'Google',
+                            dataIndex: 'number',
+                            key: 'number',
+                            width: 100,
+                        },
+                    ],
+                },
+                {
+                    title: '30.05.24',
+                    children: [
+                        {
+                            title: 'Яндекс',
+                            dataIndex: 'building',
+                            key: 'building',
+                            width: 100,
+                        },
+                        {
+                            title: 'Google',
+                            dataIndex: 'number',
+                            key: 'number',
+                            width: 100,
+                        },
+                    ],
+                }
+            ],
+        }
+    ];
+
+    const data: DataType[] = [];
+    for (let i = 0; i < 100; i++) {
+        data.push({
+            key: i,
+            name: 'википедия екатерина 1 ',
+            age: i + 1,
+            street: 'Lake Park',
+            building: '1',
+            number: 4,
+            companyAddress: 'Lake Street 42',
+            companyName: 'SoftLake Co',
+            gender: 'M',
+        });
+    }
 
     useEffect(() => {
         setTimeout(() => {
@@ -57,12 +202,20 @@ export const ResultCheckTable = () => {
                 </div>
 
                 <div className="blockData">
-                    {generation? (
+                    {generation ? (
                         <Skeleton active className={cls.skeleton}/>
                     ) : (
-                        <span>{'>'}100 - позиция в поисковой системе не входит в топ-100</span>
+                        <Table
+                            columns={columns}
+                            dataSource={data}
+                            bordered
+                            size="middle"
+                            scroll={{ x: 'calc(700px + 50%)', y: 280 }}
+                            className={cls.table}
+                        />
                     )}
                 </div>
+                <span className={cls.text}>{'>'}100 - позиция в поисковой системе не входит в топ-100</span>
             </div>
         </ConfigProvider>
     );
