0017eb67c03fad49bc377a6ded93ffee049975b5
profitrolka <turkin.2004@inbox.ru>
2024-06-06 15:13:01 +0500

    add update user

--- xetiumapi.client/src/entity/User/model/selectors/getUserAuthData.ts
+++ xetiumapi.client/src/entity/User/model/selectors/getUserAuthData.ts
@@ -2,4 +2,5 @@ import {StateSchema} from "@/app/providers/StoreProvaider";
 
 export const getUserAuthData = (state: StateSchema) => state.user?.authData
 
+export const getInitializedUser = (state: StateSchema) => state.user?.initialized
 
